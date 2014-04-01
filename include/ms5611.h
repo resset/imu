af2a5b762cbea8a44a6ac75a67082846c7de67f3
Mateusz Tomaszkiewicz <silentdemon@gmail.com>
2014-04-01 23:25:14 +0200

    MS5611 header file reorganisation

--- include/ms5611.h
+++ include/ms5611.h
@@ -6,20 +6,11 @@
 /* If pin nCSB is tied to GND then LSB is 1.*/
 #define MS5611_I2C_ADDR_LOW  0x77
 
+/*
+ * Commands.
+ */
+
 #define MS5611_CMD_RESET 0x1E
-
-#define MS5611_CMD_CONVERT_D1_256  0x40
-#define MS5611_CMD_CONVERT_D1_512  0x42
-#define MS5611_CMD_CONVERT_D1_1024 0x44
-#define MS5611_CMD_CONVERT_D1_2048 0x46
-#define MS5611_CMD_CONVERT_D1_4096 0x48
-#define MS5611_CMD_CONVERT_D2_256  0x50
-#define MS5611_CMD_CONVERT_D2_512  0x52
-#define MS5611_CMD_CONVERT_D2_1024 0x54
-#define MS5611_CMD_CONVERT_D2_2048 0x56
-#define MS5611_CMD_CONVERT_D2_4096 0x58
-
-#define MS5611_CMD_READ_ADC 0x0
 
 /* Pressure sensitivity (SENS T1).*/
 #define MS5611_CMD_READ_C0 0xA2
@@ -33,5 +24,21 @@
 #define MS5611_CMD_READ_C4 0xAA
 /* Temperature coefficient of the temperature (TEMPSENS).*/
 #define MS5611_CMD_READ_C5 0xAC
+
+/* D1 conversion.*/
+#define MS5611_CMD_CONVERT_D1_256  0x40
+#define MS5611_CMD_CONVERT_D1_512  0x42
+#define MS5611_CMD_CONVERT_D1_1024 0x44
+#define MS5611_CMD_CONVERT_D1_2048 0x46
+#define MS5611_CMD_CONVERT_D1_4096 0x48
+
+/* D2 conversion.*/
+#define MS5611_CMD_CONVERT_D2_256  0x50
+#define MS5611_CMD_CONVERT_D2_512  0x52
+#define MS5611_CMD_CONVERT_D2_1024 0x54
+#define MS5611_CMD_CONVERT_D2_2048 0x56
+#define MS5611_CMD_CONVERT_D2_4096 0x58
+
+#define MS5611_CMD_READ_ADC 0x0
 
 #endif /* _MS5611_H_ */
