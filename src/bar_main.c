7982c61e92f169ea8028f730a423afd8a188711a
Mateusz Tomaszkiewicz <silentdemon@gmail.com>
2014-04-02 21:31:36 +0200

    MS5611: coefficients C0 - C5 readout
    Very crude, though it's working.

--- src/bar_main.c
+++ src/bar_main.c
@@ -18,22 +18,49 @@
 
 uint8_t bar_val = 0;
 
+uint16_t coefficients[6];
+
 static void bar_init(void) {
 
   i2c_sensors_init();
 
-/*  uint8_t txbuf[1], rxbuf[2];
+  uint8_t txbuf[1], rxbuf[2];
+
+  i2cAcquireBus(&I2CD1);
+
+  txbuf[0] = MS5611_CMD_RESET;
+  i2cMasterTransmitTimeout(&I2CD1, MS5611_I2C_ADDR, txbuf, 1, rxbuf, 0, MS2ST(0x3000));
+
+  chThdSleepMilliseconds(10);
 
   rxbuf[0] = 0;
   rxbuf[1] = 0;
 
-  i2cAcquireBus(&I2CD1);
-  txbuf[0] = 0x75;
-  i2cMasterTransmitTimeout(&I2CD1, MS5611_I2C_ADDR, txbuf, 1, rxbuf, 1, MS2ST(0x3000));
-  rxbuf[0] = 0;
-  txbuf[0] = 0x6b;
-  i2cMasterTransmitTimeout(&I2CD1, MS5611_I2C_ADDR, txbuf, 1, rxbuf, 1, MS2ST(0x3000));
-  i2cReleaseBus(&I2CD1);*/
+  txbuf[0] = MS5611_CMD_READ_C0;
+  i2cMasterTransmitTimeout(&I2CD1, MS5611_I2C_ADDR, txbuf, 1, rxbuf, 2, MS2ST(0x3000));
+  coefficients[0] = (rxbuf[1] << 8) | rxbuf[0];
+
+  txbuf[0] = MS5611_CMD_READ_C1;
+  i2cMasterTransmitTimeout(&I2CD1, MS5611_I2C_ADDR, txbuf, 1, rxbuf, 2, MS2ST(0x3000));
+  coefficients[1] = (rxbuf[1] << 8) | rxbuf[0];
+
+  txbuf[0] = MS5611_CMD_READ_C2;
+  i2cMasterTransmitTimeout(&I2CD1, MS5611_I2C_ADDR, txbuf, 1, rxbuf, 2, MS2ST(0x3000));
+  coefficients[2] = (rxbuf[1] << 8) | rxbuf[0];
+
+  txbuf[0] = MS5611_CMD_READ_C3;
+  i2cMasterTransmitTimeout(&I2CD1, MS5611_I2C_ADDR, txbuf, 1, rxbuf, 2, MS2ST(0x3000));
+  coefficients[3] = (rxbuf[1] << 8) | rxbuf[0];
+
+  txbuf[0] = MS5611_CMD_READ_C4;
+  i2cMasterTransmitTimeout(&I2CD1, MS5611_I2C_ADDR, txbuf, 1, rxbuf, 2, MS2ST(0x3000));
+  coefficients[4] = (rxbuf[1] << 8) | rxbuf[0];
+
+  txbuf[0] = MS5611_CMD_READ_C5;
+  i2cMasterTransmitTimeout(&I2CD1, MS5611_I2C_ADDR, txbuf, 1, rxbuf, 2, MS2ST(0x3000));
+  coefficients[5] = (rxbuf[1] << 8) | rxbuf[0];
+
+  i2cReleaseBus(&I2CD1);
 
   return;
 }
