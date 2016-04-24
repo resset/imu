246581d343ea79811991beebfe8b55d3d7ff4bfc
Mateusz Tomaszkiewicz <silentdemon@gmail.com>
2016-04-24 21:38:58 +0200

    Added barometer helper script

---
+++ tools/bar.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+def bar_calc(c1, c2, c3, c4, c5, c6, d1, d2):
+    dt = d2 - (c5 << 8)
+    print 'dt = %s' % dt
+
+    temp = 2000 + ((dt * c6) >> 23)
+    print 'temp = %s' % temp
+
+    #off = (c2 << 16) + ((c4 * dt) >> 7)
+    off = (c2 << 16)
+    print 'off = %s' % off
+
+    #sens = (c1 << 15) + ((c3 * dt) >> 8)
+    sens = ((c4 * dt) >> 7)
+    print 'sens = %s' % sens
+
+    p = (((d1 * sens) >> 21) - off) >> 15
+    print 'p = %s' % p
+
+    print ''
+    print 'Temperature: %s [°C]' % (temp / 100.0) 
+    print 'Pressure: %s [mbar,hPa]' % (p / 100.0) # 1013.25
+    print ''
+
+def bar_calc_parse(string):
+    lines = string.split("\n")
+    for i in range(len(lines)):
+        if lines[i][0:2] == 'c1':
+          c1 = int(lines[i][4:])
+        if lines[i][0:2] == 'c2':
+          c2 = int(lines[i][4:])
+        if lines[i][0:2] == 'c3':
+          c3 = int(lines[i][4:])
+        if lines[i][0:2] == 'c4':
+          c4 = int(lines[i][4:])
+        if lines[i][0:2] == 'c5':
+          c5 = int(lines[i][4:])
+        if lines[i][0:2] == 'c6':
+          c6 = int(lines[i][4:])
+        if lines[i][0:2] == 'd1':
+          d1 = int(lines[i][4:])
+        if lines[i][0:2] == 'd2':
+          d2 = int(lines[i][4:])
+    bar_calc(c1, c2, c3, c4, c5, c6, d1, d2)
+    pass
+
+c1 = 40127
+c2 = 36924
+c3 = 23317
+c4 = 23282
+c5 = 33464
+c6 = 28312
+d1 = 9085466
+d2 = 8569150
+#bar_calc(c1, c2, c3, c4, c5, c6, d1, d2)
+
+c1 = 50289
+c2 = 51075
+c3 = 30840
+c4 = 28547
+c5 = 31311
+c6 = 27892
+d1 = 8406872
+d2 = 8180152
+#bar_calc(c1, c2, c3, c4, c5, c6, d1, d2)
+
+c1 = 40127
+c2 = 36924
+c3 = 23317
+c4 = 23282
+c5 = 33464
+c6 = 28312
+d1 = 9085466
+d2 = 8569150
+#bar_calc(c1, c2, c3, c4, c5, c6, d1, d2)
+
+data = """
+c1: 50289
+c2: 51075
+c3: 30840
+c4: 28547
+c5: 31311
+c6: 27892
+d1: 8457016
+d2: 8130776
+"""
+bar_calc_parse(data)
