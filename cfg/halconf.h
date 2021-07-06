20ddb68a2be74f78777208a695791ef1919969a9
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2021-07-06 14:46:06 +0200

    Fix SBUS communication
    - Changed driver from USART to SIO: USART apparently needs some tweaks to work
      properly with cache and DMA. Enabling DMA for SIO is still TODO.
    - Data is now received as a complete packets, we rely on RX IDLE interrupt to
      split them.

--- cfg/halconf.h
+++ cfg/halconf.h
@@ -156,7 +156,7 @@
  * @brief   Enables the SIO subsystem.
  */
 #if !defined(HAL_USE_SIO) || defined(__DOXYGEN__)
-#define HAL_USE_SIO                         FALSE
+#define HAL_USE_SIO                         TRUE
 #endif
 
 /**
@@ -177,7 +177,7 @@
  * @brief   Enables the UART subsystem.
  */
 #if !defined(HAL_USE_UART) || defined(__DOXYGEN__)
-#define HAL_USE_UART                        TRUE
+#define HAL_USE_UART                        FALSE
 #endif
 
 /**
