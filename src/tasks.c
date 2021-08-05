75e6aa4de78cccf05320b38bd761749f1e069c30
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2021-08-05 21:17:11 +0200

    Disable imu thread
    So I can work on the same I2C peripheral it used.

--- src/tasks.c
+++ src/tasks.c
@@ -29,7 +29,7 @@
 
 void tasks_init(void)
 {
-  chThdCreateStatic(waImu, sizeof(waImu), HIGHPRIO, thImu, NULL);
+  //chThdCreateStatic(waImu, sizeof(waImu), HIGHPRIO, thImu, NULL);
   chThdCreateStatic(waController, sizeof(waController), HIGHPRIO - 1, thController, NULL);
 
   chThdCreateStatic(waGnss, sizeof(waGnss), NORMALPRIO, thGnss, NULL);
