69e7cf4661d9bb45e019021c5d1a9579dcb7ee0e
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2021-09-07 13:53:32 +0200

    Added ICM-20689 header

--- inc/icm20689.h
+++ inc/icm20689.h
@@ -14,29 +14,51 @@
     limitations under the License.
 */
 
-#ifndef _MPU6050_H_
-#define _MPU6050_H_
-
-/* If pin AD0 is tied to GND then LSB is 0.*/
-#define MPU6050_ADDR_LOW  0x68
-/* If pin AD0 is tied to VCC then LSB is 1.*/
-#define MPU6050_ADDR_HIGH 0x69
+#ifndef _ICM20689_H_
+#define _ICM20689_H_
 
 /*
  * Registers.
  */
 
-#define MPU6050_SMPRT_DIV 0x19
-#define MPU6050_CONFIG 0x1A
-#define MPU6050_GYRO_CONFIG 0x1B
-#define MPU6050_ACCEL_CONFIG 0x1C
-#define MPU6050_FIFO_EN 0x23
-#define MPU6050_INT_ENABLE 0x38
-#define MPU6050_SIGNAL_PATH_RESET 0x68
-#define MPU6050_USER_CTRL 0x6A
-#define MPU6050_PWR_MGMT_1 0x6B
-#define MPU6050_PWR_MGMT_2 0x6C
-#define MPU6050_WHO_AM_I 0x75
-#define MPU6050_WHO_AM_I_IDENTITY 0x68
+#define ICM20689_SMPLRT_DIV 0x19
+#define ICM20689_CONFIG 0x1A
+#define ICM20689_GYRO_CONFIG 0x1B
+#define ICM20689_ACCEL_CONFIG 0x1C
+#define ICM20689_ACCEL_CONFIG2 0x1D
+#define ICM20689_FIFO_EN 0x23
+#define ICM20689_INT_ENABLE 0x38
+#define ICM20689_ACCEL_XOUT_H 0x3B
+#define ICM20689_ACCEL_XOUT_L 0x3C
+#define ICM20689_ACCEL_YOUT_H 0x3D
+#define ICM20689_ACCEL_YOUT_L 0x3E
+#define ICM20689_ACCEL_ZOUT_H 0x3F
+#define ICM20689_ACCEL_ZOUT_L 0x40
+#define ICM20689_TEMP_OUT_H 0x41
+#define ICM20689_TEMP_OUT_L 0x42
+#define ICM20689_GYRO_XOUT_H 0x43
+#define ICM20689_GYRO_XOUT_L 0x44
+#define ICM20689_GYRO_YOUT_H 0x45
+#define ICM20689_GYRO_YOUT_L 0x46
+#define ICM20689_GYRO_ZOUT_H 0x47
+#define ICM20689_GYRO_ZOUT_L 0x48
+#define ICM20689_SIGNAL_PATH_RESET 0x68
+#define ICM20689_USER_CTRL 0x6A
+#define ICM20689_PWR_MGMT_1 0x6B
+#define ICM20689_PWR_MGMT_2 0x6C
+#define ICM20689_WHO_AM_I 0x75
 
-#endif /* _MPU6050_H_ */
+/*
+ * Register values.
+ */
+
+#define ICM20689_VAL_WHO_AM_I 0x98
+
+/*
+ * Other constants.
+ */
+
+#define ICM20689_READ_MASK  0x80
+#define ICM20689_WRITE_MASK 0x00
+
+#endif /* _ICM20689_H_ */
